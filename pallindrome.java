ebabb23c44a425796ca753a78647bff9b0581266
murshidha-hash <ymurshi13@gmail.com>
2026-07-15 11:21:47 +0530

    Add files via upload

---
+++ pallindrome.java
@@ -0,0 +1,13 @@
+class  pallindrome {
+    public boolean isPalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+
+        int temp = x;
+        int sum = 0;
+        while (x != 0) {
+            int digit = x % 10;
+            sum = sum * 10 + digit;
+            x = x / 10;
+        }
